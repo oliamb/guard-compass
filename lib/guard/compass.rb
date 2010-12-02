9b4c6de5ed83fa109de0c409fba1ad36cbd81961
Stefano Verna <stefano.verna@gmail.com>
2010-12-02 14:37:26 +0100

    Added compass syntax success/error notifications

--- lib/guard/compass.rb
+++ lib/guard/compass.rb
@@ -5,58 +5,77 @@ require 'guard/reporter'
 require 'compass'
 require 'compass/commands'
 require 'compass/logger'
+require 'compass/compiler'
+
+module Compass
+  class Compiler
+    alias :old_handle_exception :handle_exception
+    def handle_exception(sass_filename, css_filename, e)
+      old_handle_exception(sass_filename, css_filename, e)
+      # rethrow the exception, we need it to notificate the user!
+      raise Sass::SyntaxError, "[#{File.basename(sass_filename)}:#{e.sass_line}] #{e.message}"
+    end
+  end
+end
 
 module Guard
   class Compass < Guard
     attr_reader :updater, :options
     attr_accessor :reporter
-    
+
     def initialize(watchers = [], options = {})
       super
       @reporter = Reporter.new
       @options[:workdir] ||= File.expand_path(File.dirname("."))
     end
-    
+
     # Guard Interface Implementation
-    
+
     # Compile all the sass|scss stylesheets
     def start
       create_updater
       reporter.announce "Guard::Compass is watching at your stylesheets."
       true
     end
-    
+
     def stop
       @updater = nil
       true
     end
-    
+
     # Reload the configuration
     def reload
       create_updater
       true
     end
-    
+
     # Compile all the sass|scss stylesheets
     def run_all
       perform
     end
-    
+
     # Compile the changed stylesheets
     def run_on_change(paths)
       perform
     end
-    
+
     private
       def perform
         if valid_sass_path?
-          @updater.execute
-          true
+          begin
+            @updater.execute
+            Notifier.notify("No errors.", :title => "Compass")
+            true
+          rescue Sass::SyntaxError => e
+            Notifier.notify(e.message, :image => :failed, :title => "Compass")
+            reporter.failure "#{e.message}"
+            false
+          end
         else
           false
         end
       end
-      
+
       def create_updater
         if(options[:configuration_file])
           filepath = Pathname.new(options[:configuration_file])
@@ -73,7 +92,7 @@ module Guard
         @updater = ::Compass::Commands::UpdateProject.new(@options[:workdir] , @options)
         valid_sass_path?
       end
-      
+
       def valid_sass_path?
         unless File.exists? ::Compass.configuration.sass_path
           reporter.failure("Sass files src directory not found: #{::Compass.configuration.sass_path}\nPlease check your Compass configuration.")
